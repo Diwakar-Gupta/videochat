f44898a3fc10bda47f7ecac9ba28d04e8371177f
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2022-10-14 20:27:47 +0530

    chat app done

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,59 @@
 # chat/consumers.py
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+
+PREFIX_CHANNEL_NAME = 'specific..inmemory!'
+
 class ChatConsumer(WebsocketConsumer):
+
     def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        self.display_channel_name = self.channel_name.split('!')[1]
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+
         self.accept()
+        
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': self.display_channel_name
+        }))
 
     def disconnect(self, close_code):
-        print('WebSocket DISCONNECTED')
-        pass
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
+    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'group_message',
+                'message': message
+            }
+        )
+
+    # Receive message from room group
+    def group_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
         self.send(text_data=json.dumps({
             'message': message
         }))
+
